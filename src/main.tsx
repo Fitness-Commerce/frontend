fb752e3b0ed32b59c10fdef15908004b8e93e00a
wdgWon <same666@naver.com>
2023-08-25 19:18:30 +0900

    feat: React-Query 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 import Home from "./pages/Home";
 import Products from "./pages/Products";
@@ -9,6 +11,7 @@ import GlobalStyles from "./style/GlobalStyles";
 import Header from "./components/header";
 import Navigation from "./components/Navigation";
 
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
 	{
@@ -37,8 +40,9 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<>
+	<QueryClientProvider client={queryClient}>
 		<GlobalStyles />
 		<RouterProvider router={router} />
-	</>
+		<ReactQueryDevtools />
+	</QueryClientProvider>
 );

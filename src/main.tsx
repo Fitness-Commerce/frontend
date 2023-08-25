9e8b00edbf5e129afed6ae1c489421303ed9a7df
wdgWon <same666@naver.com>
2023-08-25 20:36:50 +0900

    feat: Recoil 추가 및 예제코드 #17 #21

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RecoilRoot } from "recoil";
 
 import Home from "./pages/Home";
 import Products from "./pages/Products";
@@ -14,35 +15,38 @@ import Navigation from "./components/Navigation";
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element:
-		<>
-			<Header />
-			<Navigation />
-			<Outlet />
-		</>,
-		children: [
-			{
-				index: true,
-				element: <Home />
-			},
-			{
-				path: "products",
-				element: <Products />
-			},
-			{
-				path: "community",
-				element: <Community />
-			}
-		]
-	}
-])
+    {
+        path: "/",
+        element: (
+            <>
+                <Header />
+                <Navigation />
+                <Outlet />
+            </>
+        ),
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: "products",
+                element: <Products />,
+            },
+            {
+                path: "community",
+                element: <Community />,
+            },
+        ],
+    },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<QueryClientProvider client={queryClient}>
-		<GlobalStyles />
-		<RouterProvider router={router} />
-		<ReactQueryDevtools />
-	</QueryClientProvider>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+            <GlobalStyles />
+            <RouterProvider router={router} />
+            <ReactQueryDevtools />
+        </RecoilRoot>
+    </QueryClientProvider>
 );
